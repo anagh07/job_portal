195551ff35243df9baf362b2663f7745c3b9ad84
Anagh <anagh07@gmail.com>
2020-08-07 13:37:51 -0400

    setting config file for aits

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "iddqdidkfa");
-define("DB_NAME", "career_portal");
+define("DB_HOST", "hxc5531.encs.concordia.ca");
+define("DB_USER", "hxc55311");
+define("DB_PASS", "TheGoodP");
+define("DB_NAME", "hxc55311");
 
 // Page title
 define("SITE_TITLE", "Career Portal");
