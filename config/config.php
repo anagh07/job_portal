9c7391c08398496ab1163ec694295a2b67634050
Anagh Mehran <anagh07@gmail.com>
2020-08-10 17:57:59 -0400

    fix config

--- config/config.php
+++ config/config.php
@@ -1,9 +1,18 @@
 <?php
 // Database params
-define("DB_HOST", "localhost");
-define("DB_USER", "id14569282_root");
-define("DB_PASS", "KelqRPb^f61sF!9n");
-define("DB_NAME", "id14569282_career");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "id14569282_root");
+// define("DB_PASS", "KelqRPb^f61sF!9n");
+// define("DB_NAME", "id14569282_career");
+
+// // Page title
+// define("SITE_TITLE", "Career Portal");
+
+// Database params
+define("DB_HOST", "hxc5531.encs.concordia.ca");
+define("DB_USER", "hxc55311");
+define("DB_PASS", "TheGoodP");
+define("DB_NAME", "hxc55311");
 
 // Page title
 define("SITE_TITLE", "Career Portal");
